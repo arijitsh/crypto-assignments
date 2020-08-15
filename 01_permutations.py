681b00ebd5a524342d902ea450fc28668c88f154
Arijit Shaw <if.arijit@gmail.com>
2020-08-16 02:31:01 +0530

    debugging permutations

--- 01_permutations.py
+++ 01_permutations.py
@@ -1,20 +1,21 @@
 def permutation(lst):
     # print(lst)
+    if len(lst) == 0:
+        return []
     if len(lst) == 1:
-        return lst
+        return [lst]
     res = []
     for i in range(len(lst)):
         remaining = lst[:i] + lst[i+1:]
         p = permutation(remaining)
         for items in p:
-            x_p = [lst[i]] + [items]
-            # print(x_p)
+            x_p = [lst[i]] + items
             res.append(x_p)
-    # print(lst)
     return res
 
 if __name__ == "__main__":
     n = int(input("Enter n : "))
     assert(n > 0)
     nat_n = list(range(n+1))[1:]
-    print(permutation(nat_n))
+    for p in permutation(nat_n):
+        print(p)
